e33d8c5b736bd2fe78db67c2a16354a7e4b87c85
gaurav-kamti <gauravkumarleo@gmail.com>
2025-08-28 13:53:09 +0530

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TARGET = new Date("2025-08-28T13:45:00");
+const TARGET = new Date("2025-08-28T13:57:00");
 const ONTIME_DURATION = 30 * 1000;
 
 const musicBefore = document.getElementById("musicBefore");
